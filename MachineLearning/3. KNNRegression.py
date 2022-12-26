4037d870ba767e2321a7758fab89327575c2b4b6
Jiwoon <mmvv11@naver.com>
2022-12-26 14:33:39 +0900

    main branch 파일명

--- MachineLearning/3. KNNRegression.py
+++ MachineLearning/3. KNNRegression.py
@@ -1,4 +1,11 @@
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 perch_length = np.array(
     [8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0,
@@ -7,7 +14,7 @@ perch_length = np.array(
      27.3, 27.5, 27.5, 27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0,
      36.5, 36.0, 37.0, 37.0, 39.0, 39.0, 39.0, 40.0, 40.0, 40.0,
      40.0, 42.0, 43.0, 43.0, 43.5, 44.0]
-     )
+)
 perch_weight = np.array(
     [5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0,
      110.0, 115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0,
@@ -16,4 +23,10 @@ perch_weight = np.array(
      514.0, 556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0,
      820.0, 850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0,
      1000.0, 1000.0]
-     )
+)
+
+train_input, test_input, train_target, test_target = train_test_split(perch_length, perch_weight, random_state=42)
+test_array = np.array([1,2,3,4])
+test_array = test_array.reshape(2, 2)
+train_input = train_input.reshape(-1, 1)
+test_input = test_input.reshape(-1, 1)
