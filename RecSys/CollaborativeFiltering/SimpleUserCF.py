897e895fe0dd20e8affd0e0f502ecbedfeb46ec1
jiwoon <mmvv11@naver.com>
2023-01-11 22:37:32 +0900

    user based cf 코드 분석

--- RecSys/CollaborativeFiltering/SimpleUserCF.py
+++ RecSys/CollaborativeFiltering/SimpleUserCF.py
@@ -20,17 +20,20 @@ data = ml.loadMovieLensLatestSmall()
 
 trainSet = data.build_full_trainset()
 
-sim_options = {'name': 'cosine',
+
+sim_options = {'name': 'cosine', # 코사인 유사도를 기반.. MSD, 피어슨 모두 가능
                'user_based': True
                }
 
 model = KNNBasic(sim_options=sim_options)
 model.fit(trainSet)
 simsMatrix = model.compute_similarities()
+# print(f"simsMatrix: {simsMatrix[84]}")
 
 # Get top N similar users to our test subject
 # (Alternate approach would be to select users up to some similarity threshold - try it!)
 testUserInnerID = trainSet.to_inner_uid(testSubject)
+print(f"testUserInnerID:{testUserInnerID}")
 similarityRow = simsMatrix[testUserInnerID]
 
 similarUsers = []
@@ -39,6 +42,7 @@ for innerID, score in enumerate(similarityRow):
         similarUsers.append( (innerID, score) )
 
 kNeighbors = heapq.nlargest(k, similarUsers, key=lambda t: t[1])
+print(f"kNeighbors: {kNeighbors}") # 비슷한 유저의 id와 유사도를 저장
 
 # Get the stuff they rated, and add up ratings for each item, weighted by user similarity
 candidates = defaultdict(float)
@@ -46,14 +50,16 @@ for similarUser in kNeighbors:
     innerID = similarUser[0]
     userSimilarityScore = similarUser[1]
     theirRatings = trainSet.ur[innerID]
+    print(f"theirRatings: {theirRatings}")
     for rating in theirRatings:
         candidates[rating[0]] += (rating[1] / 5.0) * userSimilarityScore
-    
+
+print(f"candidates: {candidates}")
 # Build a dictionary of stuff the user has already seen
 watched = {}
 for itemID, rating in trainSet.ur[testUserInnerID]:
     watched[itemID] = 1
-    
+
 # Get top-rated items from similar users:
 pos = 0
 for itemID, ratingSum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
