67ab12884a27479c83c83c458dd12b2a02754859
jiwoon <mmvv11@naver.com>
2023-01-16 11:21:52 +0900

    flask 주석 생성

--- FER/FlaskServer.py
+++ FER/FlaskServer.py
@@ -1,5 +1,3 @@
-# https://howtolivelikehuman.tistory.com/118
-
 from flask import Flask, render_template, request, send_file
 import cv2
 import numpy as np
@@ -7,59 +5,58 @@ from keras.models import model_from_json
 from io import BytesIO
 from flask_cors import CORS
 
-
-
 app = Flask(__name__)
 CORS(app)
 
+
 def get_res(file):
-
-    # TODO: 모델 결과 돌려주기
-
-    img = file.read()
-    img =  cv2.imdecode(np.fromstring(img, np.uint8), cv2.IMREAD_UNCHANGED)
-
-    ## 기존 코드
+    # 감정 class
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-    # load json and create model
+    # 이미지 파일 로드, cv2 포맷으로 디코드
+    img = file.read()
+    img = cv2.imdecode(np.fromstring(img, np.uint8), cv2.IMREAD_UNCHANGED)
+    # 이미지 사이즈 조절 및 gray 스케일 전환
+    frame = img
+    # frame = cv2.resize(frame, (1024, 768))
+    frame = cv2.resize(frame, (1280, 720))
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 모델 로드
     json_file = open('model/emotion_model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     emotion_model = model_from_json(loaded_model_json)
-
-    # load weights into new model
+    # 모델 weight 로드
     emotion_model.load_weights("model/emotion_model.h5")
-    # img = cv2.imread(file)
-    frame = img
-    frame = cv2.resize(frame, (1280, 720))
-    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # detect faces available on camera
+    # 얼굴 감지 객체 로드
+    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+    # 감지된 얼굴 객체
     num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
-    # take each face available on the camera and Preprocess it
+    # 감지된 얼굴 객체 -> 모델을 통한 감정 분석 -> 이미지에 결과 그리기
     for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (0, 255, 0), 4)
+        # 사각형 그리기
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # 얼굴 부분만 추출
         roi_gray_frame = gray_frame[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
 
-        # predict the emotions
+        # 얼굴 -> 모델, 감정 분석
         emotion_prediction = emotion_model.predict(cropped_img)
         print(f"emotion_prediction: {emotion_prediction}")
+
+        # 감정 -> 이미지에 그리기
         text = ""
-
+        dy = 30
         for i, v in enumerate(emotion_prediction[0]):
-            # v = v * 100
-            text += f"{emotion_dict[i]}:{v}\n"
-
-        y0, dy = 50, 25
+            text += f"{emotion_dict[i]}:{round(float(v), 4)}\n"
         for i, line in enumerate(text.split('\n')):
-            ny = y0 + i * dy
-            # cv2.rectangle(frame, (x + w + 5, ny), (x+10, ny+10), (0,0,0), -1)
+            ny = y + i * dy
             cv2.putText(frame, line, (x + w + 5, ny), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
 
+    # 결과, cv2 포멧 -> jpg 형식으로 인코딩
     _, result = cv2.imencode('.jpg', frame)
     # ndarray -> 파일 객체로 형변환
     result = BytesIO(result)
@@ -68,21 +65,21 @@ def get_res(file):
 
 @app.route("/")
 def main():
-    # templates 폴더 안에서 찾음
+    # templates 폴더에서 탐색된 파일을 기준
     return render_template('index.html')
 
 
-
-@app.route('/prediction', methods=['POST'])
+@app.route('/analysed-emotions', methods=['POST'])
 def predict():
     try:
         file = request.files['image']
         result = get_res(file)
-        print(type(result))
         return send_file(result, mimetype='image/jpeg')
     except Exception as e:
         print(e)
+        # templates 폴더에서 탐색된 파일을 기준
         return render_template('error.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- FER/readme.md
+++ FER/readme.md
@@ -4,13 +4,19 @@ https://www.youtube.com/watch?v=UHdrxHPRBng
 FER2013 Dataset  
 https://www.kaggle.com/datasets/msambare/fer2013
 
+### Face Emotions Recognition
+#####before
+![before](./testdata/woman-g0f7d9994a_1920.jpg)
+####after
+![after](./testdata/after.png)
+
 ### 프로젝트 구조
 ㄴdataset: FER2013 Dataset, gitignore 등록되어 있음. 위 링크를 통해 직접 설치  
 ㄴhaarcascades: 하르캐스케이드, 얼굴 탐지 객체  
 ㄴmodel: 학습된 DNN 모델  
 ㄴtemplates: flask server가 제공하는 정적파일  
 ㄴtestdata: test-img파일에서 참조하는 테스트 이미지  
-FlaskSever.py: 플라스크 서버
+FlaskSever.py: 플라스크 서버  
 test-img.py: 학습된 모델을 이용하여 얼굴 감정 인식 테스트  
 train.py: 모델 학습
 
