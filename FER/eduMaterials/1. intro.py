e7d2da21de23425541d41bb113080b4402b7f5a8
jiwoon <mmvv11@naver.com>
2023-01-21 18:07:06 +0900

    fer 플젝 intro 자료 제작

---
+++ FER/eduMaterials/1. intro.py
@@ -0,0 +1,28 @@
+from tensorflow import keras
+
+# Define the model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Load the MNIST dataset
+mnist = keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Fit the model to the training data
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model on the test data
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print('Test accuracy:', test_acc)
