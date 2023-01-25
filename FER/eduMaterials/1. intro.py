359861527ffde0930abb8b87074aee620a8e58c2
jiwoon <mmvv11@naver.com>
2023-01-25 18:01:42 +0900

    딥러닝 스터디 4. 수치해석 작업중

--- FER/eduMaterials/1. intro.py
+++ FER/eduMaterials/1. intro.py
@@ -1,4 +1,8 @@
 from tensorflow import keras
+import matplotlib.pyplot as plt
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Define the model
 model = keras.Sequential([
@@ -20,9 +24,19 @@ mnist = keras.datasets.mnist
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
+# Choose an image to display
+image_index = 0
+# Display the image
+plt.imshow(x_test[image_index], cmap='gray')
+plt.show()
+
+# TODO 1. model.predict
+
+
 # Fit the model to the training data
 model.fit(x_train, y_train, epochs=5)
 
 # Evaluate the model on the test data
 test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"predict: {model.predict(x_test[image_index])}")
 print('Test accuracy:', test_acc)
